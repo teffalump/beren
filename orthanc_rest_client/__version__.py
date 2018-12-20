69ce881a2bfe13669375d41d8a573d6414150275
teffalump <chris@teffalump.com>
2018-12-19 18:31:50 -0800

    Version bump for url change

--- orthanc_rest_client/__version__.py
+++ orthanc_rest_client/__version__.py
@@ -11,6 +11,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-VERSION = (0, 2, 2)
+VERSION = (0, 2, 3)
 
 __version__ = '.'.join(map(str, VERSION))
