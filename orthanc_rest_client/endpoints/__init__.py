a43d3b1f228adc176a9d6ddc223f84406c8f6abb
teffalump <chris@teffalump.com>
2018-12-20 18:27:23 -0800

    Restructure

--- orthanc_rest_client/endpoints/__init__.py
+++ orthanc_rest_client/endpoints/__init__.py
@@ -1,7 +1,3 @@
 from .patients import OrthancPatients
 
-__all__=['Orthanc']
-
-class Orthanc:
-    def __init__(self, domain, *args, **kwargs):
-        self.patients = OrthancPatients(domain, *args, **kwargs)
+__all__=['OrthancPatients']
