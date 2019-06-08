20867d5c675e2af0ea49998cdda1b2201e94befb
teffalump <chris@teffalump.com>
2019-06-08 09:27:59 -0700

    Lint tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,8 @@
 from orthanc_rest_client import Orthanc
 
 
-URL = 'http://demo.orthanc-server.com'
+URL = "http://demo.orthanc-server.com"
+
 
 def test_server_target():
     orthanc = Orthanc(URL)
